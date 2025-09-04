a67e6bb3f14558234a74493ab4d139246ca0ffbd
Nepellion <nepellion@proton.me>
2025-09-05 00:12:53 +0200

    Add missing dates to metadata

--- Jellyfin.Plugin.Kitsu/Providers/KitsuIO/ApiClient/Models/KitsuSeriesAttributes.cs
+++ Jellyfin.Plugin.Kitsu/Providers/KitsuIO/ApiClient/Models/KitsuSeriesAttributes.cs
@@ -7,7 +7,8 @@ namespace Jellyfin.Plugin.Kitsu.Providers.KitsuIO.ApiClient.Models
         public string Synopsis { get; set; }
         public KitsuTitles Titles { get; set; }
         public string AverageRating { get; set; }
-        public DateTimeOffset? StartDate { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
         public KitsuImage PosterImage { get; set; }
         public KitsuImage CoverImage { get; set; }
     }
